2d8bae975e24cc007dfc9838df4fdb817bc09e38
saivinay2223 <saivinaykumarbikki@gmail.com>
2024-11-05 20:50:41 -0600

    updated grid.js and grid.pug

--- routes/grid.js
+++ routes/grid.js
@@ -1,8 +1,16 @@
 // grid.js
-app.get('/grid', (req, res) => {
-    let query = req.query;
-    console.log(`rows ${query.rows}`);
-    console.log(`cols ${query.cols}`);
-    res.render('grid', { title: "Grid Display", query: query });
-  });
+const express = require('express');
+const router = express.Router();
+
+router.get('/gridbuild', (req, res) => {
+  let query = req.query;
+  console.log(`Rows: ${query.rows}`);
+  console.log(`Cols: ${query.cols}`);
   
+  res.render('grid', {
+    title: "Grid Display",
+    query: query
+  });
+});
+
+module.exports = router;
