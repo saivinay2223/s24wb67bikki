77b798ac7f95873fda5c8ab464205096018024ec
saivinay2223 <saivinaykumarbikki@gmail.com>
2024-11-06 13:21:10 -0600

    updated app.js

--- app.js
+++ app.js
@@ -9,33 +9,40 @@ var usersRouter = require('./routes/users');
 var galaxiesRouter = require('./routes/galaxies');
 var gridRouter = require('./routes/grid'); // Import the grid router
 var pickRouter = require('./routes/pick'); // Import the pick router for random item selection
+var searchResultsRouter = require('./routes/searchResults'); // Import the searchResults router
 
 var app = express();
 
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Route definitions
 app.use('/', indexRouter);            // Root route
 app.use('/users', usersRouter);        // Users route
 app.use('/galaxies', galaxiesRouter);  // Galaxies route
 app.use('/grid', gridRouter);          // Grid route
 app.use('/randomitem', pickRouter);    // Random item selection route
+app.use('/search-results', searchResultsRouter); // Search results route
+
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-
+// Error handler
 app.use(function(err, req, res, next) {
-
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
