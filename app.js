e57177c6299971f92385a78b0698786fa41941e4
saivinay2223 <saivinaykumarbikki@gmail.com>
2024-11-06 19:45:24 -0600

    updated app.js

--- app.js
+++ app.js
@@ -9,15 +9,13 @@ var usersRouter = require('./routes/users');
 var galaxiesRouter = require('./routes/galaxies');
 var gridRouter = require('./routes/grid');
 var pickRouter = require('./routes/pick');
-const searchResultsRouter = require('./routes/searchResults'); // Import the searchResults router
+var searchResultsRouter = require('./routes/searchResults'); 
 
 var app = express();
 
-// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,8 +28,7 @@ app.use('/users', usersRouter);
 app.use('/galaxies', galaxiesRouter);
 app.use('/grid', gridRouter);
 app.use('/randomitem', pickRouter);
-app.use('/searchResults', searchResultsRouter); // Search results route
-
+app.use('/searchResults', searchResultsRouter); 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -42,7 +39,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // Render the error page
+ 
   res.status(err.status || 500);
   res.render('error');
 });
